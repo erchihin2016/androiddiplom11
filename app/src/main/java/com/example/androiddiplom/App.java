adafcb1c131a95800e8977613d188049b4fb4d27
SergioRamos4 <erchihin2016@yandex.ru>
2020-09-01 14:24:47 +0300

    room пропал

--- app/src/main/java/com/example/androiddiplom/App.java
+++ app/src/main/java/com/example/androiddiplom/App.java
@@ -1,19 +1,26 @@
 package com.example.androiddiplom;
 
 import android.app.Application;
+import android.os.Build;
 
-import com.example.locknote.database.DaggerStorageComponent;
-import com.example.locknote.database.StorageComponent;
-import com.example.locknote.database.StorageModule;
+import androidx.annotation.RequiresApi;
+
+import com.example.androiddiplom.database.DaggerStorageComponent;
+import com.example.androiddiplom.database.StorageComponent;
+import com.example.androiddiplom.database.StorageModule;
+
+import java.util.stream.DoubleStream;
 
 public class App extends Application {
 
     private static StorageComponent component;
+    private DoubleStream DaggerStorageComponent;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onCreate() {
         super.onCreate();
-        component = DaggerStorageComponent
+        component = DoubleStream
                 .builder()
                 .storageModule(new StorageModule(this))
                 .build();
--- app/src/main/java/com/example/androiddiplom/database/NoteDao.java
+++ app/src/main/java/com/example/androiddiplom/database/NoteDao.java
@@ -6,7 +6,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
-import com.example.locknote.Note;
+import com.example.androiddiplom.Note;
 
 import java.util.List;
 
