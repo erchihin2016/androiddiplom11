d238454d84dce675a688c750cf57e8458ec4d3fd
SergioRamos4 <erchihin2016@yandex.ru>
2020-10-04 14:05:08 +0300

    NoteComparator.java

---
+++ app/src/main/java/com/example/androiddiplom/NoteComparator.java
@@ -0,0 +1,40 @@
+package com.example.androiddiplom;
+
+import android.text.TextUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
+public class NoteComparator implements Comparator<Note> {
+
+    private final DateFormat format = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault( ));
+
+    @Override
+    public int compare(final Note first, Note second) {
+        try{
+            if (TextUtils.isEmpty(first.getNoteDeadline()) && !TextUtils.isEmpty(second.getNoteDeadline())) {
+                return 1;
+            }
+            if (TextUtils.isEmpty(first.getNoteDeadline())) {
+                return 0;
+            }
+            if (TextUtils.isEmpty(second.getNoteDeadline())) {
+                return -1;
+            }
+
+            Date firstDeadline = format.parse(first.getNoteDeadline());
+            Objects.requireNonNull(firstDeadline);
+            Date secondDeadline = format.parse(second.getNoteDeadline());
+            return firstDeadline.compareTo(secondDeadline);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+}
